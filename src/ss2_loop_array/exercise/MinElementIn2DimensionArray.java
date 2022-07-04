f1a12a0df043ed9666d85cc1634de606fb1defba
thuytiendang <thuytiendang2003@gmail.com>
2022-07-04 20:45:02 +0700

    m

--- src/ss2_loop_array/exercise/MinElementIn2DimensionArray.java
+++ src/ss2_loop_array/exercise/MinElementIn2DimensionArray.java
@@ -2,35 +2,33 @@ package ss2_loop_array.exercise;
 
 import java.util.Scanner;
 
-public class MaxElementIn2DimensionArray {
+public class MinElementIn2DimensionArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Input row");
-        int row = scanner.nextInt();
-        System.out.print("Input col");
-        int col = scanner.nextInt();
+        System.out.println("input row");
+        int row = Integer.parseInt(scanner.nextLine());
+        System.out.println("input col");
+        int col = Integer.parseInt(scanner.nextLine());
         int[][] arr = new int[row][col];
-
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                System.out.printf("input arr[%d][%d] = ", i, j);
+                System.out.printf("input arr[%d][%d]= ", i, j);
                 arr[i][j] = scanner.nextInt();
             }
         }
-        System.out.println("matrix:");
-        int max = arr[0][0];
+        System.out.println("matrix");
+        int min = arr[0][0];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j] + " ");
-                if (arr[i][j] > max) {
-                    max = arr[i][j];
+                if (arr[i][j] < min){
+                    min =  arr[i][j];
                 }
             }
             System.out.println();
         }
-        System.out.println(" max element in array:");
-        System.out.println(max);
-
+        System.out.println("min element in array:");
+        System.out.println(min);
     }
 }
